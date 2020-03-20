128ec5784456d0003e3077dbda4e973efd90581c
Juan Manuel Garcia <jguillemat@essiprojects.com>
2020-03-20 16:29:11 +0100

    Modified backup.sh

--- backup.sh
+++ backup.sh
@@ -1,118 +1,94 @@
 #!/bin/bash
 
 # EXIT ERRORS
-readonly E_MNTFS=255              # ISSUE MOUNTING FILESYSTEM.
-readonly E_EMPTYFS=254            # FILESYSTEM IS EMPTY
 readonly E_NOPOD=253              # CANNOT GET POD NAME
+readonly E_NOVOLUME=252           # POD_VOLUME not set
 readonly E_NOERROR=0              # ALL IT's OK
 
-
-#FUNCTIONS
-
+# --------------------------------------
+# FUNCTIONS
+# --------------------------------------
 is_empty() {
-  local var="${1}"
-  local empty=1
+    local var="${1}"
+    local empty=1
 
-  if [[ -z "${var}" ]]; then
-    empty=0
-  fi
-
-  return "${empty}"
-}
-
-get_restrictive_id(){
-  local fs="${1}"
-  local id=""
-
-  id="$( find ${fs} -maxdepth 1 -type f ! -perm -g+r ! -perm -o+r ! -path '*/\.*' -exec stat -c '%u:%g' {} \; | awk 'FNR == 1' )"
-
-  echo "${id}"
-}
-
-get_id(){
-  local fs="${1}"
-  local id=""
-
-  id="$( find ${fs} -maxdepth 1 -type f -exec stat -c '%u:%g' {} \; | awk 'FNR == 1' )"
-
-  echo "${id}"
+    if [[ -z "${var}" ]]; then
+        empty=0
+    fi
+    return "${empty}"
 }
 
 get_pod_name(){
 	
-  local selector="${1}"
-  local project="${2}"
+    local selector="${1}"
+    local project="${2}"
 
-  local pod_name=""
+    local pod_name=""
 
-  if [[ -z "${project}" ]]; then
-	pod_name=$(oc get po --namespace=$project --selector=$selector --no-headers -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.name}{"\n"}{end}' | head -n1)
-  else
-      	pod_name=$(oc get po --selector=$selector --no-headers -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.name}{"\n"}{end}' | head -n1)
-  fi
-
-  echo "${pod_name}"	  
+    if [[ -z "${project}" ]]; then
+        pod_name=$(oc get po --namespace=$project --selector=$selector --no-headers -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.name}{"\n"}{end}' | head -n1)
+    else
+        pod_name=$(oc get po --selector=$selector --no-headers -o jsonpath='{range .items[?(@.status.phase=="Running")]}{.metadata.name}{"\n"}{end}' | head -n1)
+    fi
+    echo "${pod_name}"	  
 }
 
-#FUNCTIONS
+# --------------------------------------
+# MAIN METHOD
+# --------------------------------------
 main () {
 
-  local -r src="/source"
-  local -r dst="/backup"
+    local -r src="/source"
+    local -r dst="/backup"
 
-  local -r pod_name="${POD_NAME}"
-  local -r volume_data="${POD_VOLUME}"
-  local -r selector="${SELECTOR}"
-  local -r project="${PROJECT}"
+    local -r pod_name="${POD_NAME}"
+    local -r volume_data="${POD_VOLUME}"
+    local -r pod_selector="${POD_SELECTOR}"
+    local -r project="${PROJECT}"
 
+    local -r bck_dir="${BCK_FOLDER}"
 
-  local -r bck_dir="${BCK_FOLDER}"
-  
-  echo "Getting Pod Name"
-  if [[ "${pod_name}" == "" ]]; then
-    pod_name="$( get_pod_name ${selector} ${project} )"
-    if [[ "${pod_name}" == "" ]]; then
-       echo "ERROR: CANNOT GET POD_NAME. Exit."
-	exit "${E_NOPOD}"
-    fi  
-  fi
-
-
-  local src_dir="${src}/"
-  if ! is_empty "${bck_dir}" && [[ -d "${src}/${bck_dir}" ]] ; then
-    src_dir="${src}/${bck_dir}/"
-  fi
-
-  dst_dir="${dst}/"
-
-  user_ugid="$( get_restrictive_id ${src_dir} )"
- 
-  if [[ "${user_ugid}" == "" ]]; then
-    user_ugid="$( get_id ${src_dir} )"
-
-    if [[ "${user_ugid}" == "" ]]; then
-      echo "The file system/folder ${src_dir} is empty" 
-      exit "${E_EMPTYFS}"
+    echo "Checking Pod Volume to backup...."
+    if [[ "${volume_data}" == "" ]]; then
+        echo "ERROR: POD_VOLUME not specified. Exit."
+        exit "${E_NOVOLUME}"
     fi
 
-  fi
+    echo "Checking Volume to store data...."
+    if [[ "${bck_dir}" == "" ]]; then
+        bck_dir="/data-replica"
+    fi
+           
+    echo "Checking Pod Selector...."
+    if [[ "${pod_selector}" == "" ]]; then
+        echo "Setting default pod selector -> docker-registry=default" 
+        pod_selector="docker-registry=default"
+    fi        
 
-  user_uid=$(echo $user_ugid | cut -d':' -f1)
-  user_gid=$(echo $user_ugid | cut -d':' -f2)
+ 
+    echo "Getting Pod Name"
+    if [[ "${pod_name}" == "" ]]; then
+        pod_name="$( get_pod_name ${selector} ${project} )"
+        if [[ "${pod_name}" == "" ]]; then
+            echo "ERROR: CANNOT GET POD_NAME. Exit."
+            exit "${E_NOPOD}"
+        fi  
+    else
+        echo "Specified POD_NAME=${pod_name} "
+    fi
 
+    dst_dir="${bck_dir}/"
 
-  if [[ "${project}" == "" ]]; then
-	  echo "Start OC RSYNC ..."
-	  oc rsync ${pod_name}:${src_dir} ${dst_dir} --progress 
-  else 
-	  echo "Start OC RSYNC from POD  into project ${project} ..."
-	  oc rsync ${pod_name}:${src_dir} ${dst_dir} --progress --namespace=${project}
-  fi
-  echo "Ended OC RSYNC."
-
+    if [[ "${project}" == "" ]]; then
+        echo "Start OC RSYNC from POD ${pod_name} into {dst_dir  ..."
+        # oc rsync ${pod_name}:${src_dir} ${dst_dir} --progress 
+    else 
+        echo "Start OC RSYNC from POD ${pod_name} of project ${project} into {dst_dir..."
+        # oc rsync ${pod_name}:${src_dir} ${dst_dir} --progress --namespace=${project}
+    fi
+    echo "End OC RSYNC."
 }
 
 
 main "$@"
-
 exit "${E_NOERROR}"
